9a0c16c74068a789b80b0601ade11709052fbc38
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-04-14 12:52:25 +0200

    With the build...

---
+++ build/unbundled/bower_components/ace-builds/src-noconflict/snippets/snippets.js
@@ -0,0 +1,1 @@
+ace.define("ace/snippets/snippets",["require","exports","module"],function(a,b){"use strict";b.snippetText="# snippets for making snippets :)\nsnippet snip\n\tsnippet ${1:trigger}\n\t\t${2}\nsnippet msnip\n\tsnippet ${1:trigger} ${2:description}\n\t\t${3}\nsnippet v\n\t{VISUAL}\n",b.scope="snippets"});
